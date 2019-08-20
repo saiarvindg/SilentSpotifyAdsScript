5f3823f679d7b7e5c3955d2cc03c71ea90022228
Sai Arvind <sai.arvindg@gmail.com>
2019-08-20 00:00:26 -0400

    basics are working - ads muted and songe unmuted

--- spotify.py
+++ spotify.py
@@ -1,12 +1,21 @@
 import spotipy
+from datetime import datetime, timedelta
 
 def get_curr_track(token):
 	sp = spotipy.Spotify(auth=token)
-	return sp.current_user_playing_track()
+	curr_track = dict(sp.current_user_playing_track())
+	if curr_track is None or curr_track["currently_playing_type"] is None:
+		return (False, "Could not get the currently playing track")
+	
+	return (True, curr_track)
 
-def get_curr_track_type(token):
-	sp = spotipy.Spotify(token)
-	return sp.current_playback()
-
-def calc_track_end_time(remaining_ms, track_type):
-	print("remaining ms: " + str(remaining_ms) + " for track: " +track_type)
+# credit: https://stackoverflow.com/questions/11523918/python-start-a-function-at-given-time
+def calc_track_end_time(total_duration, curr_progress):
+	"""
+	Returns the time the current track will end in seconds
+	"""
+	time_left = total_duration - curr_progress
+	now = datetime.now()
+	run_at = now + timedelta(milliseconds=time_left)
+	delay = (run_at - now).total_seconds()
+	return delay
--- ssas.py
+++ ssas.py
@@ -1,27 +1,68 @@
 import sys
 import json
+import time
+from datetime import datetime, timedelta
+
 from auth import *
 from spotify import *
+from volume import *
 
 token = ""
 
+ad_length = 30000  # assume ads are only 30 seconds since the spotify api does not give ad lengths :(
+
 
 def run(token):
-	curr_track = get_curr_track(token) # currently_playing_type
-	print(curr_track)
-	print("\n")
-	track_type = get_curr_track_type(token)
-	print(track_type)
-	print("DONE")
+	print("Getting currently playing track...")
+	result, curr_track = get_curr_track(token)  # currently_playing_type
+	if result:
+		track_type = curr_track["currently_playing_type"]
+		curr_progress = curr_track["progress_ms"]
+		if track_type == "track":
+			# song
+			set_curr_volume(75)  # TODO: get the current volume and set it to that
+			song = curr_track["item"]["name"]
+			artists = [item["name"]
+                            for item in curr_track["item"]["artists"] if item["name"]]
+			duration = curr_track["item"]["duration_ms"]
+			print(f"Currenty playing track is: {song} by " + ", ".join(
+				[str(a) for a in artists]) + f" lasting for {duration} ms with {curr_progress} ms already played")
+			print(
+				f"spotify api will be called in {duration - curr_progress} ms to check for the next track")
+			return (True, calc_track_end_time(duration, curr_progress))
+		elif track_type == "ad":
+			# ad
+			print(
+				f"currenty playing an ad for lasting for {ad_length - curr_progress} ms. Spotify will be muted for the duration")
+			set_curr_volume(0)  # TODO: save the current volume
+			return (True, calc_track_end_time(ad_length, curr_progress))
+		else:
+			print(
+				f"A valid track type was not returned - result: {result}, curr_track: {curr_track} ")
+			return (False, -1)
+	else:
+		print(
+			f"could not get current track b/c: ${curr_track}. Spotify may not be playing.")
+		return (False, -1)
 
 
-if not check_env_set():
-	print("Please ensure the client id, client secret, username, and redirect url are set in the environment")
-	sys.exit(1)
-result, msg, auth_token = get_auth_token()
-if not result:
-	print(msg)
-	sys.exit(1)
+run_result = True
+while run_result:
+	if not check_env_set():
+		print("Please ensure the client id, client secret, username, and redirect url are set in the environment")
+		sys.exit(1)
 
-token = auth_token
-run(token)
+	result, msg, auth_token = get_auth_token()
+	if not result:
+		print(msg)
+		sys.exit(1)
+
+	run_result, time_to_next_call = run(auth_token)
+	print("Run result: " + str(run_result))
+	print("Time to next call: " + str( datetime.now() + timedelta(seconds=time_to_next_call) ) + "\n")
+	if run_result:
+		time.sleep(time_to_next_call)
+	else:
+		print(
+			f"run failed - run_result: {run_result}, time to next call: {time_to_next_call}")
+		sys.exit()
